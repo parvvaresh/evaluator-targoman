3b2aac0f19fd4036cee70f6e06674b18f0bafed3
Alireza Parvaresh <89921883+parvvaresh@users.noreply.github.com>
2023-10-18 20:49:37 +0330

    Delete api-targoman directory

--- api-targoman/targoman.py
+++
@@ -1,8 +0,0 @@
-from get_data import translate
-
-def Translate() -> str:
-    word = input("please enter word who wants  to be translated : ")
-    fromLang = input("from lan  : ")
-    toLang = input("to lan : ")
-    if fromLang == "fa" and toLang == "en":
-        return translate(word, toLang=toLang, fromLang=fromLang)["result"]["tr"]
