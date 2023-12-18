55cde8a9c0714770c1cd7c72a23a5f6580de26bb
Alireza Parvaresh <89921883+parvvaresh@users.noreply.github.com>
2023-12-18 11:16:23 +0330

    Create pre_process.py

---
+++ src/data/pre_process.py
@@ -0,0 +1,10 @@
+class pre_process:
+    def __init__(self, df):
+        self.df = df
+    
+    
+    def pre_process(self, text):
+        text = text.strip()        
+        return text
+    
+    
