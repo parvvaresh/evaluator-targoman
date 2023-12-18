a5af0ae73c35f86da6acaee721cc6fa44e327f61
Alireza Parvaresh <89921883+parvvaresh@users.noreply.github.com>
2023-12-18 11:14:02 +0330

    Update filter.py

--- src/data/filter.py
+++ src/data/filter.py
@@ -1,8 +1,16 @@
-def filter_lenght(data, col_1, col_2):
-    data["col_1 lenght"] = data[col_1].apply(lambda sent: sent.split())
-    data["col_2 lenght"] = data[col_2].apply(lambda sent: sent.split())
+import pandas as pd
 
-    data = data[data["col_1 lenght"].map(len) >= 6]
-    data = data[data["col_2 lenght"].map(len) >= 6]
 
-    return data[[col_1, col_2]]
+def filter_lenght(data, columns, min_lenght):
+    for col in columns:
+        data = _filter_lenght(data, col, min_lenght)
+
+    return data
+
+
+def _filter_lenght(data, col, min_lenght):
+    data["col lenght"] = data[col].apply(lambda sent: sent.split())
+
+    data = data[data["col lenght"].map(len) >= min_lenght]
+    data = data.drop(columns=["col lenght"])
+    return data
